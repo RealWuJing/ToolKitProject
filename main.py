0cfccd74c55619a8e789f043e92bf527829f532d
wujing <178955347@qq.com>
2020-12-25 21:34:19 +0800

    放弃多线程

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from mysql.join import join_sql
 import threading
 
 if __name__ == '__main__':
-    # join_sql()
-    join_sql_thread = threading.Thread(target=join_sql)
-    join_sql_thread.start()
+    join_sql()
+    # join_sql_thread = threading.Thread(target=join_sql)
+    # join_sql_thread.start()
     # join_sql_thread.join()
     # print('main end')
