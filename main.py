c1d46dd9513bf13731455d02beeb4fb565fa325d
wujing <178955347@qq.com>
2020-12-25 22:05:15 +0800

    放弃多线程

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from toolkit.toolkit_cfg import toolkit_cfg
 from mysql.join import join_sql
+from mysql.join import join_sql2
 import threading
 
 if __name__ == '__main__':
-    join_sql()
+    # join_sql()
     # join_sql_thread = threading.Thread(target=join_sql)
     # join_sql_thread.start()
     # join_sql_thread.join()
     # print('main end')
+
+    join_sql2()
--- mysql/join.py
+++ mysql/join.py
@@ -22,6 +22,19 @@ def join_sql():
                 print(mysql)
                 f.writelines(f'\n\n{mysql}\n')
 
+def join_sql2():
+    mysql_dir = toolkit_cfg()['mysql_dir']
+    now_listdir = set(os.listdir(mysql_dir))
+    for mysql_sql in now_listdir:
+        mysql_sql_path = os.path.join(mysql_dir, mysql_sql)
+        # print(mysql_sql_path)
+        with open(mysql_sql_path, 'a+', encoding='utf8') as f:
+            f.seek(0, 0)
+            mysql = f.read()
+            mysql = ' '.join([i.strip().split('\n')[0] for i in mysql.strip().split('\n')])
+            print(mysql)
+            f.writelines(f'\n\n{mysql}\n')
+
 
 
 
